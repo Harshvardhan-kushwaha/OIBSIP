efde33a9b42d9e3854df44b03565c7773514fec6
Harshvardhan-kushwaha <harshkushwaha981@gmail.com>
2024-11-10 13:30:26 +0530

    update all code

--- TASK-1-calculator/script.js
+++ TASK-1-calculator/script.js
@@ -1,22 +1,26 @@
+function appendEliment(value) {
+  document.getElementById("display").value += value;
+  
+}
+
+
+
+function deleteLast() {
+  let deleteLast = document.getElementById("display").value;
+  document.getElementById("display").value = deleteLast.slice(0, -1);
+}
+
 function clearDisplay() {
-    document.getElementById('display').value = '';
-}
-
-function deleteLast(){
-    let currentvalue = document.getElementById('display').value;
-    document.getElementById('display').value = currentvalue.slice(0,-1);   
-}
-
-function appedEliment(){
-    document.getElementById('display').value += value;
+  document.getElementById("display").value = "";
 }
 
 function calculateResult() {
-    let result = document.getElementById('display').value;
-     try{
-        document.getElementById('display').value = eval(result);
+  let result = document.getElementById("display").value;
+  try {
+    document.getElementById("display").value = eval(result);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 
-    } catch (error){
-        document.getElementById('display').value  = 'Error';
-    }
-}
+  }
+
